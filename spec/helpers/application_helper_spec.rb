902240263f956a6c4bc03ac15fa398eab180c2cc
ywenli <kuota0204@gmail.com>
2020-06-12 09:26:47 +0000

    Add test for full_title method #1

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "#full_title" do
+    context "page_title is empty" do
+      it "removes symbol" do
+        expect(helper.full_title).to eq('Fuwa')
+      end
+    end
+    
+    context "page_title is not empty" do
+      it "returns title and application name where contains symbol" do
+        expect(helper.full_title('hoge')).to eq('hoge | Fuwa')
+      end
+    end
+  end
+  
 end
