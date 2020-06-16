477b0ed53785e27c6e7151d5aed76af559792a92
ywenli <kuota0204@gmail.com>
2020-06-16 12:13:12 +0000

    Add user_params method in private #18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
