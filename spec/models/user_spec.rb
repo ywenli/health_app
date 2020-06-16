6cca817fe220347e9b9353939c2e7877a0055203
ywenli <kuota0204@gmail.com>
2020-06-16 05:29:15 +0000

    Add test for email #11

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,11 +13,12 @@ RSpec.describe User, type: :model do
     end
   end
     
-  descrive "User.name" do
+  describe "User.name" do
     it "should be present" do
       user.name = " "
       expect(user).to be_invalid
     end
+  end
     context "when 50 characters or less" do
       it "should be valid" do
         user.name = "a" * 50
@@ -30,6 +31,45 @@ RSpec.describe User, type: :model do
         expect(user).to be_invalid
       end
     end
-    
+  
+  describe "email" do
+    it "should be present" do
+      user.email = " "
+      expect(user).to be_invalid
+    end
+    context "when 255 characters or less" do
+      it "should be valid" do
+        user.email = "a" * 243 + "@example.com"
+        expect(user).to be_valid
+      end
+    end
+    context "when 256 characters or more" do
+       it "should be invalid" do
+        user.email = "a" * 244 + "@example.com"
+        expect(user).to be_invalid
+      end
+    end
+    it "should be valid format" do
+      valid_addresses = %w[user@example.com 
+                          USER@foo.COM 
+                          A_US-ER@foo.bar.org
+                          first.last@foo.jp
+                          alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        expect(user).to be_valid
+      end
+    end
+    it "should be unique" do
+      duplicate_user = user.dup
+      duplicate_user.email = user.email.upcase
+      user.save!
+      expect(duplicate_user).to be_invalid
+    end
+    it "should be saved as lower-case" do
+      user.email = "FoO@eXaMple.CoM"
+      user.save!
+      expect(user.reload.email).to eq 'foo@example.com'
+    end
   end
 end
