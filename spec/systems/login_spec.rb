b348f6c013eb97bd98d4105e2a51b7ce488ad05a
ywenli <kuota0204@gmail.com>
2020-06-17 06:09:09 +0000

    Add test for valid login #22

--- spec/systems/login_spec.rb
+++ spec/systems/login_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Logins", type: :system do
       fill_in 'パスワード', with: ''
       click_button 'ログイン'
     end
-    it "has a flash message" do
+    it "has a flash error message" do
       expect(current_path).to eq login_path
       expect(page).to have_selector '.login-container'
       expect(page).to have_selector '.alert_danger'
@@ -18,5 +18,17 @@ RSpec.describe "Logins", type: :system do
       expect(page).to_not have_selector '.alert_danger'
     end 
   end
-    
+  
+  context "with valid information" do
+    before do
+      visit login_path
+      fill_in 'メールアドレス', with: user.email
+      fill_in 'パスワード', with: 'password'
+      click_button 'ログイン'
+    end
+    it "is valid login" do
+      expect(current_path).to eq user_path(1)
+      expect(page).to have_selector '.show-container'
+    end
+  end
 end
