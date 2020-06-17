2f66ce30bb22408d1c3bb715758e75eae4023ab3
ywenli <kuota0204@gmail.com>
2020-06-17 09:35:02 +0000

    Add some method in user.rb #26

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :downcase_email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
@@ -16,6 +17,23 @@ class User < ApplicationRecord
   validates :password,
     presence: true,
     length: { minimum: 6 }
+  
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
+  
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
   
   private
     def downcase_email
