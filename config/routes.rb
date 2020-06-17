d379081fb6d52cf0109b5f388cbbd7a50601a717
ywenli <kuota0204@gmail.com>
2020-06-17 03:34:55 +0000

    Add login form view #22

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
   resources :users
 end
