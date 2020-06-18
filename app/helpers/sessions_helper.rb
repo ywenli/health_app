9a003f734aba3b23376f47df130cf9f100cb519c
ywenli <kuota0204@gmail.com>
2020-06-18 02:23:03 +0000

    Add a conditional branch between permanent and temporary sessions #26

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,12 +10,19 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # 記憶トークンcookieに対応するユーザーを返す
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_if = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    els (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
-  
+    
   def logged_in?
     !current_user.nil?
   end
