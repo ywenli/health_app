abf03273dc5611c82a79c961d621225f90d7d51a
ywenli <kuota0204@gmail.com>
2020-06-18 00:44:58 +0000

    Add some method #26

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   def current_user
     if session[:user_id]
       @current_user ||= User.find_by(id: session[:user_id])
