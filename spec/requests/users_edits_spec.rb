b0f7beb818d33332935b8b4e0c3bbf2d1bdf07f6
ywenli <kuota0204@gmail.com>
2020-06-19 14:45:49 +0000

    Add test for other_user's edit #28

--- spec/requests/users_edits_spec.rb
+++ spec/requests/users_edits_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "UsersEdits", type: :request do
   let(:user) { create(:user) }
+  let(:other_user) { create(:other_user) }
   before { log_in_as(user) }
   
   # patch: 情報の一部を渡す
@@ -12,7 +13,7 @@ RSpec.describe "UsersEdits", type: :request do
                                             password_confirmation: "bar" } }
   end
   
-  describe "GET /users/:id/edit" do
+  describe "edit" do
     context "with invalid info" do
       it "does not works and redirect to user page" do
         expect(is_logged_in?).to be_truthy
@@ -32,6 +33,15 @@ RSpec.describe "UsersEdits", type: :request do
         follow_redirect!
         expect(request.fullpath).to eq '/users/1/edit'
       end
+    end
+  end
+  
+  describe "other_user" do
+    it "cannot edit user's settings" do
+      log_in_as(other_user)
+      get edit_user_path(user)
+      follow_redirect!
+      expect(request.fullpath).to eq '/'
     end
   end
 end
