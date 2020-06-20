0938484143a0c85df99b6fbb3e831549228f1888
ywenli <kuota0204@gmail.com>
2020-06-20 05:46:04 +0000

    Add account activation code to the User model #31

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   validates :name, 
@@ -46,6 +47,12 @@ class User < ApplicationRecord
   
   private
     def downcase_email
-      email.downcase!
+      self.email = email.downcase
+    end
+    
+    # 有効化トークンとダイジェストを作成及び代入する
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
     end
 end
